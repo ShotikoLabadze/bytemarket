9f203de9af6cc3484aded5824bb1dcccd3ad4c35
ShotaLaabb <shotikoslabadzes1@gmail.com>
2026-02-06 18:15:19 +0400

    feat: implement auth middleware logic

--- backend/src/routes/ProductRoutes.js
+++ backend/src/routes/ProductRoutes.js
@@ -1,7 +1,16 @@
 import ProductController from "../controllers/ProductController.js";
 import express from "express";
+import { AdminMiddleware } from "../middleware/AdminMiddleware.js";
+import { AuthMiddleware } from "../middleware/AuthMiddleware.js";
 const router = express.Router();
 
 router.get("/", ProductController.getProducts);
+router.get("/search", ProductController.searchProducts);
+router.post(
+  "/",
+  AuthMiddleware,
+  AdminMiddleware,
+  ProductController.createProduct,
+);
 
 export default router;
